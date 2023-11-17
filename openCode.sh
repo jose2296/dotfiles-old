11ebf479ca9648bdf4910e6d1b1c17045a4e9b2f
Jose Jerez <josejerez2296@gmail.com>
2023-11-17 11:19:53 +0100

    Update openCode.sh

--- openCode.sh
+++ openCode.sh
@@ -3,7 +3,7 @@
 all=$(ls ~/workspace)
 important="api\nplatform\nengineering\nclient-portal\nclient-portal-v2\ncommercial-simulator\ninstallers-app\nmonitoring-app"
 k8="pro\npre\ndev"
-options="important\nall\nk8\nvpns"
+options="important\nall\nk8\nvpns\ncode"
 
 # optionSelected=$(echo $options | fzf)
 optionSelected=$(echo $options | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2" -color-active "#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36" -color-urgent "#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2" -show -dmenu)
@@ -30,6 +30,13 @@ dir=$(echo $k8 | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#
 if [ "$dir" != "" ]
 then
     code ~/workspace/$dir
+fi
+elif [ "$optionSelected" = "code" ]
+then
+dir=$(ls ~/code | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2" -color-active "#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36" -color-urgent "#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2" -show -dmenu)
+if [ "$dir" != "" ]
+then
+    code ~/code/$dir
 fi
 else
 echo "No option selected."

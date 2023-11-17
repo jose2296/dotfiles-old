ab5ba89f63bba6399fb9f065efe824b2d9683093
Jose Jerez <josejerez2296@gmail.com>
2023-11-17 11:18:36 +0100

    Create openCode.sh

---
+++ openCode.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+all=$(ls ~/workspace)
+important="api\nplatform\nengineering\nclient-portal\nclient-portal-v2\ncommercial-simulator\ninstallers-app\nmonitoring-app"
+k8="pro\npre\ndev"
+options="important\nall\nk8\nvpns"
+
+# optionSelected=$(echo $options | fzf)
+optionSelected=$(echo $options | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2" -color-active "#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36" -color-urgent "#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2" -show -dmenu)
+
+# rofiCommand="rofi -color-window \"#282a36, #000000, #44475a\" -color-normal \"#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2\" -color-active \"#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36\" -color-urgent \"#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2\" -show -dmenu"
+
+if [ "$optionSelected" = "important" ]
+then
+dir=$(echo $important | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2" -color-active "#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36" -color-urgent "#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2" -show -dmenu)
+if [ "$dir" != "" ]
+then
+    code ~/workspace/$dir
+fi
+elif [ "$optionSelected" = "all" ]
+then
+dir=$(echo $all | sed "s/ /\n/g" | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2" -color-active "#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36" -color-urgent "#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2" -show -dmenu)
+if [ "$dir" != "" ]
+then
+    code ~/workspace/$dir
+fi
+elif [ "$optionSelected" = "k8" ]
+then
+dir=$(echo $k8 | rofi -color-window "#282a36, #000000, #44475a" -color-normal "#282a36, #40fa7b, #44475a, #bd93f9, #f8f8f2" -color-active "#ff79c6, #f8f8f2, #ffb86c, #8be9fd, #282a36" -color-urgent "#ff5555, #f8f8f2, #ff5555, #ff5555, #f8f8f2" -show -dmenu)
+if [ "$dir" != "" ]
+then
+    code ~/workspace/$dir
+fi
+else
+echo "No option selected."
+fi
